99cae1745ac8f6e9fdb84e7b16b542d3b8b76742
omangbaheti <60500898+omangbaheti@users.noreply.github.com>
2021-05-24 13:13:53 +0530

    Update SimulateGravity.cs

--- Assets/Scripts/SimulateGravity.cs
+++ Assets/Scripts/SimulateGravity.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SimulateGravity : MonoBehaviour
 {
 
-    [SerializeField] private readonly float GravitationalConstant = 6.67f;
+    [SerializeField] private readonly float gravitationalConstant = 6.67f;
     private Rigidbody2D rb;
 
     private void Start()
@@ -14,13 +14,13 @@ public class SimulateGravity : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag != "Player")
             return;
         Rigidbody2D rbAttract = other.GetComponent<Rigidbody2D>();
-        Vector2 direction =  (rb.position - rbAttract.position).normalized;
+        Vector2 direction =  (rb.position - rbAttract.position);
         float distance = direction.magnitude;
-        float forceMagnitude = GravitationalConstant * (rbAttract.mass * rb.mass) / Mathf.Pow(distance, 2);
-        rbAttract.AddForce(direction * forceMagnitude );
+        float forceMagnitude = gravitationalConstant * (rbAttract.mass * rb.mass) / Mathf.Pow(distance, 2);
+        rbAttract.AddForce(direction.normalized * forceMagnitude );
 
     }
 
